52f9dcc2775a1ecfe5b5ebec014be03d7f1c23d1
宋晓奎 <51503996+songxiaokui@users.noreply.github.com>
2022-05-17 14:10:57 +0800

    [fix]修改swagger-ui依赖的渲染样式资源为云存储对象

--- cmd/swagger/commands/serve.go
+++ cmd/swagger/commands/serve.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"path"
 	"strconv"
+	"strings"
 
 	"github.com/go-openapi/loads"
 	"github.com/go-openapi/runtime/middleware"
@@ -18,17 +19,22 @@ import (
 	"github.com/toqueteos/webbrowser"
 )
 
+// 国内云端存储的swagger-ui的样式文件网络下载地址前缀
+const DefaultSwaggerCloudStaticFileUrLPrefix string = "https://unpkg.com/swagger-ui-dist"
+const DefaultRedocCloudStaticFileUrLPrefix string = "https://cdn.jsdelivr.net/npm/redoc/bundles"
+
 // ServeCmd to serve a swagger spec with docs ui
 type ServeCmd struct {
-	BasePath string `long:"base-path" description:"the base path to serve the spec and UI at"`
-	Flavor   string `short:"F" long:"flavor" description:"the flavor of docs, can be swagger or redoc" default:"redoc" choice:"redoc" choice:"swagger"`
-	DocURL   string `long:"doc-url" description:"override the url which takes a url query param to render the doc ui"`
-	NoOpen   bool   `long:"no-open" description:"when present won't open the the browser to show the url"`
-	NoUI     bool   `long:"no-ui" description:"when present, only the swagger spec will be served"`
-	Flatten  bool   `long:"flatten" description:"when present, flatten the swagger spec before serving it"`
-	Port     int    `long:"port" short:"p" description:"the port to serve this site" env:"PORT"`
-	Host     string `long:"host" description:"the interface to serve this site, defaults to 0.0.0.0" default:"0.0.0.0" env:"HOST"`
-	Path     string `long:"path" description:"the uri path at which the docs will be served" default:"docs"`
+	BasePath  string `long:"base-path" description:"the base path to serve the spec and UI at"`
+	Flavor    string `short:"F" long:"flavor" description:"the flavor of docs, can be swagger or redoc" default:"redoc" choice:"redoc" choice:"swagger"`
+	DocURL    string `long:"doc-url" description:"override the url which takes a url query param to render the doc ui"`
+	NoOpen    bool   `long:"no-open" description:"when present won't open the the browser to show the url"`
+	NoUI      bool   `long:"no-ui" description:"when present, only the swagger spec will be served"`
+	Flatten   bool   `long:"flatten" description:"when present, flatten the swagger spec before serving it"`
+	Port      int    `long:"port" short:"p" description:"the port to serve this site" env:"PORT"`
+	Host      string `long:"host" description:"the interface to serve this site, defaults to 0.0.0.0" default:"0.0.0.0" env:"HOST"`
+	Path      string `long:"path" description:"the uri path at which the docs will be served" default:"docs"`
+	SourceUrL string `long:"source_url" description:"specifies the path to the swaager-ui render style file download url prefix" short:"S"`
 }
 
 // Execute the serve command
@@ -78,12 +84,26 @@ func (s *ServeCmd) Execute(args []string) error {
 
 	visit := s.DocURL
 	handler := http.NotFoundHandler()
+
+	// redoc格式使用
+	var reDocUrlPrefix string
+	// swagger-ui样式使用
+	var swaggerUiUrlPrefix string
+	if s.SourceUrL != "" {
+		reDocUrlPrefix = s.SourceUrL
+		swaggerUiUrlPrefix = s.SourceUrL
+	} else {
+		reDocUrlPrefix = DefaultRedocCloudStaticFileUrLPrefix
+		swaggerUiUrlPrefix = DefaultSwaggerCloudStaticFileUrLPrefix
+	}
+
 	if !s.NoUI {
 		if s.Flavor == "redoc" {
 			handler = middleware.Redoc(middleware.RedocOpts{
 				BasePath: basePath,
 				SpecURL:  path.Join(basePath, "swagger.json"),
 				Path:     s.Path,
+				RedocURL: strings.Join([]string{reDocUrlPrefix, "redoc.standalone.js"}, "/"),
 			}, handler)
 			visit = fmt.Sprintf("http://%s:%d%s", sh, sp, path.Join(basePath, "docs"))
 		} else if visit != "" || s.Flavor == "swagger" {
@@ -91,6 +111,12 @@ func (s *ServeCmd) Execute(args []string) error {
 				BasePath: basePath,
 				SpecURL:  path.Join(basePath, "swagger.json"),
 				Path:     s.Path,
+				// Update swagger-ui default config
+				SwaggerURL:       strings.Join([]string{swaggerUiUrlPrefix, "swagger-ui-bundle.js"}, "/"),
+				SwaggerPresetURL: strings.Join([]string{swaggerUiUrlPrefix, "swagger-ui-standalone-preset.js"}, "/"),
+				SwaggerStylesURL: strings.Join([]string{swaggerUiUrlPrefix, "swagger-ui.css"}, "/"),
+				Favicon16:        strings.Join([]string{swaggerUiUrlPrefix, "favicon-16x16.png"}, "/"),
+				Favicon32:        strings.Join([]string{swaggerUiUrlPrefix, "favicon-32x32.png"}, "/"),
 			}, handler)
 			visit = fmt.Sprintf("http://%s:%d%s", sh, sp, path.Join(basePath, s.Path))
 		}
